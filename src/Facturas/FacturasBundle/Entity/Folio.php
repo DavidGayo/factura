6c1ed89be0ac0446b71580c6c657b6790d653bae
DavidGayo <crowin@hotmail.com>
2015-12-21 19:25:30 -0600

    folio

--- src/Facturas/FacturasBundle/Entity/Folio.php
+++ src/Facturas/FacturasBundle/Entity/Folio.php
@@ -47,6 +47,26 @@ class Folio
      */
     private $modelo;
 
+    /** 
+    * @var string 
+    */
+    private $llave;
+
+    /** 
+    * @var sting
+    */
+    private $llavePath;
+
+    /** 
+    * @var string
+    */
+    private $certificado;
+
+    /** 
+    * @var string
+    */
+    private $certificadoPath;
+
 
     /**
      * Get id
@@ -225,4 +245,160 @@ class Folio
     {
         return $this->modelo;
     }
+
+    /** 
+    * Set llave
+    *
+    * @param string $llave
+    *
+    * @return Folio
+    */
+    public function setLlave($llave)
+    {
+        $this->llave = $llave;
+
+        return $this;
+    }
+
+    /** 
+    * Get llave
+    *
+    * @return string
+    */
+    public function getLlave()
+    {
+        return $this->llave;
+    }
+
+    /** 
+    * Set llavePath
+    *
+    * @param string $llavePath
+    * 
+    * @return Folio
+    */
+    public function setLlavePath($llavePath)
+    {
+        $this->llavePath = $llavePath;
+
+        return $this;
+    }
+
+    /** 
+    * Get llavePath
+    *
+    * @return string
+    */
+    public function getLlavePath()
+    {
+        return $this->llavePath;
+    }
+
+    /**
+    * Set certificado
+    *
+    * @param string $certificado
+    *
+    * @return Folio
+    */
+    public function setCertificado($certificado)
+    {
+        $this->certificado = $certificado;
+
+        return $this;
+    }
+
+    /** 
+    * Get certificado
+    *
+    * @return string
+    */
+    public function getCertificado()
+    {
+        return $this->certificado;
+    }
+
+    /** 
+    * Set certificadoPath
+    *
+    * @param string $certificadoPath
+    *
+    * @return Folio
+    */
+    public function setCertificadoPath($certificadoPath)
+    {
+        $this->certificadoPath = $certificadoPath;
+
+        return $this;
+    }
+
+    /** 
+    * Get certificadoPath
+    *
+    * @return Folio
+    */ 
+    public function getCertificadoPath()
+    {
+        return $this->certificadoPath;
+    }
+
+    /**
+     * Manages the copying of the file to the relevant place on the server
+     */
+    public function upload($dir='Archivos')
+    {
+        // the file property can be empty if the field is not required
+        if (null === $this->getLlave()) {
+            return false;
+        }
+
+        // we use the original file name here but you should
+        // sanitize it at least to avoid any security issues
+
+        // move takes the target directory and target filename as params
+        $this->getLlave()->move(
+            $this->getUploadRootDir($dir),
+            urlencode(time().$this->getLlave()->getClientOriginalName())
+        );
+
+        // set the path property to the filename where you've saved the file
+        $this->llavePath = urlencode(time().$this->getLlave()->getClientOriginalName());
+        $this->llave=$this->getUploadDir($dir).'/'.$this->llavePath;
+        return true;
+        // clean up the f$this->path = $this->getFile()->getClientOriginalName();ile property as you won't need it anymore
+        //$this->setImagen(null);
+    }
+
+     /**
+     * Updates the hash value to force the preUpdate and postUpdate events to fire
+     */
+   
+    public function getAbsolutePath()
+    {
+        return null === $this->llavePath
+            ? null
+            : $this->getUploadRootDir().'/'.$this->llavePath;
+    }
+
+    public function getWebPath()
+    {
+        return null === $this->llavePath
+            ? null
+            : $this->getUploadDir().'/'.$this->llavePath;
+    }
+
+    protected function getUploadRootDir($dir='Archivos')
+    {
+        // the absolute directory path where uploaded
+        // documents should be saved
+        return __DIR__.'/../../../../../web/'.$this->getUploadDir($dir);
+    }
+
+    protected function getUploadDir($dir='Archivos')
+    {
+        // get rid of the __DIR__ so it doesn't screw up
+        // when displaying uploaded doc/image in the view.
+        return 'uploads/'.$dir;
+    }
+
 }
